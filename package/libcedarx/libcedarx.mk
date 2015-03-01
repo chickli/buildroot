c0b514a53f5529c10fa4f4bbc73c25d9b5937a36
cli <cli@cli-OptiPlex-9010.(none)>
2015-03-01 14:46:38 +0800

    Try using git hub for libcedarx

--- package/libcedarx/libcedarx.mk
+++ package/libcedarx/libcedarx.mk
@@ -1,3 +1,10 @@
 #
 # libcedarx
 #
+
+LIBCEDARX_VERSION = master
+LIBCEDARX_SITE = $(call github,willswang,libcedarx,$(FOO_VERSION))
+LIBCEDARX_INSTALL_STAGING = YES
+LIBCEDARX_INSTALL_TARGET = YES
+
+$(eval $(autotools-package))
